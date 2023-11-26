b1f4dcca80943348f406e5b651d30a90456b4e4f
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2023-11-26 20:45:27 +0900

    :pencil2: 移動

---
+++ motiontweensample.js
@@ -0,0 +1,25 @@
+window.frameEvents = [
+  {
+    type: "putImage",
+    putImage: {
+      source:
+        "https://blog.ta.kasaki.info/wp-content/uploads/2022/06/IMG_20220522_182837929-1152x1536.jpg",
+    },
+    depth: 1,
+    frameCount: 20,
+    layoutOptions: {
+      x: 0,
+      y: 0,
+      width: 120,
+      height: 120,
+    },
+    lastKeyFrame: {
+      layoutOptions: {
+        x: 50,
+        y: 50,
+        width: 120,
+        height: 120,
+      },
+    },
+  },
+];
