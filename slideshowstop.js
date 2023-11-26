bdb161592453b83845bc5cab41973ea771490f26
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2023-11-26 18:22:39 +0900

    :pencil2: 停止の実装

---
+++ slideshowstop.js
@@ -0,0 +1,66 @@
+window.frameEvents = [
+  {
+    type: "putImage",
+    putImage: {
+      source:
+        "https://blog.ta.kasaki.info/wp-content/uploads/2022/06/IMG_20220522_182837929-1152x1536.jpg",
+    },
+    depth: 1,
+    frameCount: 0,
+    layoutOptions: {
+      x: 0,
+      y: 0,
+      width: 120,
+      height: 120,
+    },
+  },
+  {
+    type: "doNothing",
+    frameCount: 10,
+  },
+  {
+    type: "putImage",
+    putImage: {
+      source:
+        "https://blog.ta.kasaki.info/wp-content/uploads/2022/06/IMG_20220524_122351263-1536x1152.jpg",
+    },
+    depth: 1,
+    frameCount: 0,
+    layoutOptions: {
+      x: 20,
+      y: 20,
+      width: 120,
+      height: 120,
+    },
+  },
+  {
+    type: "doNothing",
+    frameCount: 10,
+  },
+  {
+    type: "putImage",
+    putImage: {
+      source:
+        "https://blog.ta.kasaki.info/wp-content/uploads/2021/06/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88-2021-06-03-14.28.33.png",
+    },
+    depth: 1,
+    frameCount: 0,
+    layoutOptions: {
+      x: 40,
+      y: 40,
+      width: 120,
+      height: 120,
+    },
+  },
+  {
+    type: "doNothing",
+    frameCount: 10,
+  },
+  {
+    type: "executeAction",
+    frameCount: 0,
+    executeAction: {
+      type: 'stop',
+    }
+  },  
+];
