ad0f775c0ababb6fd849d711de3ff655e848d595
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2023-11-26 17:49:28 +0900

    :pencil2: 切り出した

---
+++ depthsample.js
@@ -0,0 +1,86 @@
+window.frameEvents = [
+  {
+    type: "putImage",
+    putImage: {
+      source:
+        "https://blog.ta.kasaki.info/wp-content/uploads/2022/06/IMG_20220522_182837929-1152x1536.jpg",
+    },
+    depth: 1,
+    frameCount: 1,
+    layoutOptions: {
+      x: 0,
+      y: 0,
+      width: 120,
+      height: 120,
+    },
+    motionTweenEnabled: true,
+  },
+  {
+    type: "doNothing",
+    frameCount: 9,
+  },
+  {
+    type: "putImage",
+    putImage: {
+      source:
+        "https://blog.ta.kasaki.info/wp-content/uploads/2022/06/IMG_20220524_122351263-1536x1152.jpg",
+    },
+    depth: 1,
+    frameCount: 1,
+    layoutOptions: {
+      x: 20,
+      y: 20,
+      width: 120,
+      height: 120,
+    },
+    motionTweenEnabled: true,
+  },
+  {
+    type: "doNothing",
+    frameCount: 9,
+  },
+  {
+    type: "putImage",
+    putImage: {
+      source:
+        "https://blog.ta.kasaki.info/wp-content/uploads/2021/06/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88-2021-06-03-14.28.33.png",
+    },
+    depth: 1,
+    frameCount: 1,
+    layoutOptions: {
+      x: 40,
+      y: 40,
+      width: 120,
+      height: 120,
+    },
+    motionTweenEnabled: true,
+  },
+  {
+    type: "doNothing",
+    frameCount: 9,
+  },
+  {
+    type: "putImage",
+    putImage: {
+      source:
+        "https://blog.ta.kasaki.info/wp-content/uploads/2022/06/IMG_20220522_182837929-1152x1536.jpg",
+    },
+    depth: 1,
+    frameCount: 5,
+    layoutOptions: {
+      x: 0,
+      y: 0,
+      width: 120,
+      height: 120,
+    },
+    lastKeyFrame: {
+      layoutOptions: {
+        x: 50,
+        y: 50,
+        width: 120,
+        height: 120,
+      },
+    },
+    motionTweenEnabled: true,
+  },
+];
