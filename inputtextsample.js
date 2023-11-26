0e5a9f7bb180655e8b5535665e3968d6da1b8302
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2023-11-26 21:03:52 +0900

    :heavy_plus_sign: ファイル追加

---
+++ inputtextsample.js
@@ -0,0 +1,104 @@
+window.frameEvents = [
+  {
+    type: "putText",
+    putText: {
+      content: "number",
+      borderColor: "#000000",
+      borderStyle: "solid",
+      borderWidth: "0",
+    },
+    depth: 10,
+    frameCount: 0,
+    layoutOptions: {
+      x: 200,
+      y: 200,
+      width: 60,
+      height: 30,
+    },
+  },
+  {
+    type: "putText",
+    putText: {
+      content: "",
+      borderColor: "#000000",
+      borderStyle: "solid",
+      borderWidth: "1",
+      editable: true,
+    },
+    objectId: 'inputValue',
+    depth: 11,
+    frameCount: 0,
+    layoutOptions: {
+      x: 300,
+      y: 200,
+      width: 60,
+      height: 30,
+    },
+  },
+  {
+    type: "putText",
+    putText: {
+      content: "Result",
+      borderColor: "#000000",
+      borderStyle: "solid",
+      borderWidth: "0",
+    },
+    depth: 12,
+    frameCount: 0,
+    layoutOptions: {
+      x: 200,
+      y: 250,
+      width: 60,
+      height: 30,
+    },
+  },
+  {
+    type: "putText",
+    putText: {
+      content: "",
+      borderColor: "#000000",
+      borderStyle: "solid",
+      borderWidth: "1",
+      editable: true,
+    },
+    objectId: 'result',
+    depth: 13,
+    frameCount: 0,
+    layoutOptions: {
+      x: 300,
+      y: 250,
+      width: 60,
+      height: 30,
+    },
+  },
+  {
+    type: "putText",
+    putText: {
+      content: "Calc",
+      borderColor: "#000000",
+      borderStyle: "solid",
+      borderWidth: "1",
+    },
+    depth: 14,
+    frameCount: 0,
+    layoutOptions: {
+      x: 200,
+      y: 300,
+      width: 60,
+      height: 30,
+    },
+    onClickAction: {
+      type: "executeScript",
+      executeScript: {
+        content: 'alert(123)'
+      }
+    },
+  },
+  {
+    type: "executeAction",
+    frameCount: 1,
+    executeAction: {
+      type: "stop",
+    },
+  },
+];
