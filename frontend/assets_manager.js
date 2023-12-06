c583af5a337e59a59c5648845132be7a9f80df17
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2023-12-06 23:47:06 +0900

    :pencil2: 切り出した

---
+++ frontend/assets_manager.js
@@ -0,0 +1,17 @@
+const AssetsManager = function (initialItems) {
+  const instance = {};
+  instance.items = initialItems;
+  instance.find = function (id) {
+    return instance.items[id];
+  };
+  instance.allIdToAsset = function () {
+    return this.items;
+  };
+  instance.add = function (object) {
+    const maxId = Math.max.apply(null, Object.keys(instance.items));
+    const newId = maxId + 1;
+    instance.items[newId] = object;
+    return newId;
+  };
+  return instance;
+};
