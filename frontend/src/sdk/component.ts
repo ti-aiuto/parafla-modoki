1adebf806fff6051561f8e7373d9447c2ed5d2d4
TiAiuto (Dynabook) <ti.aiuto@outlook.jp>
2023-12-10 17:35:14 +0900

    :heavy_plus_sign: 追加

--- frontend/src/sdk/component.ts
+++ frontend/src/sdk/component.ts
@@ -5,12 +5,34 @@ import { RootController } from "./root-controller";
 import { ScreenObjectsManager } from "./screen-object-manager";
 
 export class Component {
+  rootController: RootController;
+  componentSource: ComponentSource;
+  screenObjectsManager: ScreenObjectsManager;
+  renderer: Renderer;
+
+  currentFrameCount: number = 1;
+  jumpToFrameCount: number | null = null;
+  lastFrameCount: number;
+  rootInstanceId: string = "rootDummyHoge";
+  componentUserVariables: { [key in string]: any } = {};
+  componentUserFunctions: { [key in string]: string } = {};
+
   constructor(rootController: RootController,
     componentSource: ComponentSource,
     screenObjectsManager: ScreenObjectsManager,
     renderer: Renderer
   ) {
+    this.rootController = rootController;
+    this.componentSource = componentSource;
+    this.screenObjectsManager = screenObjectsManager;
+    this.renderer = renderer;
 
+    this.lastFrameCount = Math.max.apply(
+      null,
+      Object.keys(componentSource.scheduledEvents).map((item) => Number(item))
+    );
+
+    this.componentUserFunctions = componentSource.componentUserFunctions;
   }
 
   handleAction(action: Action) {
